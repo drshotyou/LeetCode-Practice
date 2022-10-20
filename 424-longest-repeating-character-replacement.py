a02a9a7fbc7e513fa17281d0d0c2e7bcc48cafd5
Alden Rivera <alden-rf@hotmail.com>
2022-10-20 12:49:28 -0700

    Added optimized version of 424

--- 424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement.py
@@ -23,4 +23,27 @@ class Solution:
             r+=1
         return res
         
-#O(n) solution
+#O(n) solution Yeah i dont understand the maxf situation
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        sCount = {}
+        l = 0
+        r = 0
+        maxf = 0
+        res = 0
+        while (r < len(s)):
+            # print(sCount)
+            sCount[s[r]] = 1 + sCount.get(s[r],0)
+            maxf = max(maxf, sCount[s[r]])
+            # maxC = max(sCount.values())
+            windowLenReps = (r-l)+1 
+            if(windowLenReps - maxf <= k ):
+                res = windowLenReps
+                if(r == len(s)-1):
+                    break
+            else:
+                sCount[s[l]] = sCount.get(s[l],0) - 1
+                l += 1
+            r+=1
+        return res
+        
