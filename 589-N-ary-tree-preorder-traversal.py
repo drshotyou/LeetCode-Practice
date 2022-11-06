a423c999ed8f53e27fe9373e8a58d132a56e7b55
Alden Rivera <alden-rf@hotmail.com>
2022-11-06 14:51:06 -0800

    Update 589-N-ary-tree-preorder-traversal.py

--- 589-N-ary-tree-preorder-traversal.py
+++ 589-N-ary-tree-preorder-traversal.py
@@ -20,3 +20,26 @@ def traverse(root,output):
             for i in range(len(root.children)):
                 output = traverse(root.children[i],output)
     return output
+
+#v2 removes unnecesarry calls and/or variables
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+# root left right
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        nodes = []
+        return preOrderTraverse(root,nodes)
+
+def preOrderTraverse(root,nodes):
+    if(root):
+        nodes.append(root.val)
+        if(root.children):
+            for i in (root.children):
+                nodes = preOrderTraverse(i,nodes)
+    # print(nodes)
+    return nodes
