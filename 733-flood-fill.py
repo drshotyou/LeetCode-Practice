a1739bf157f1567abbec5e5a53526affde3d8fd2
Alden Rivera <alden-rf@hotmail.com>
2022-11-10 02:50:54 -0800

    Update 733-flood-fill.py

--- 733-flood-fill.py
+++ 733-flood-fill.py
@@ -10,9 +10,6 @@ class Solution:
 #               +sr,sc
         
 def replaceColor(img,sr,sc,color,ogColor):
-    # print(len(img),len(img[0]))
-    # print(sr,sc)
-    # print(img)
     if sr-1 >= 0:
         if(img[sr-1][sc]!=color and img[sr-1][sc]==ogColor):
             # print('-sr,sc')
@@ -36,3 +33,25 @@ def replaceColor(img,sr,sc,color,ogColor):
             # print('sr,+sc')
             img = replaceColor(img,sr,sc+1,color,ogColor)
     return img
+
+#V2 Better than 91%, checks all conditions first, then runs
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ogColor = image[sr][sc]
+        
+        def flood_fill(x, y):
+            if x < 0 or x >= len(image): return
+            if y < 0 or y >= len(image[0]): return
+            
+            if image[x][y] == color: return
+            if image[x][y] != ogColor: return
+            
+            image[x][y] = color
+            
+            flood_fill(x-1, y)
+            flood_fill(x+1, y)
+            flood_fill(x, y+1)
+            flood_fill(x, y-1)
+        
+        flood_fill(sr, sc)
+        return image
