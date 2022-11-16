78e9327a5646dcf6b9dfe9405e903c751e6a1081
Alden Rivera <alden-rf@hotmail.com>
2022-11-16 00:52:57 -0800

    added/updated 299 and 392

--- 299-bulls-and-cows.py
+++ 299-bulls-and-cows.py
@@ -22,3 +22,34 @@ class Solution:
                     secretCount.pop(guess[i])
         # print(secretCount)
         return (str(x) + 'A' + str(y) + 'B')
+
+
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        secretCount = collections.Counter(secret)
+        guessCount = collections.Counter(guess)
+        bulls = 0
+        cows = 0
+        for i in range(len(secret)):
+            # bulls
+            if (secret[i] == guess[i] and secretCount[secret[i]] > 0 and guessCount[guess[i]] > 0):
+                bulls+=1
+                secretCount[secret[i]] -= 1
+                guessCount[guess[i]] -= 1
+                if(secretCount[secret[i]]==0):
+                    secretCount.pop(secret[i])
+                if(guessCount[guess[i]]==0):
+                    guessCount.pop(guess[i])
+
+
+        # for i in range(len(secret)):
+        for i in range(len(guess)):
+            if (guess[i] in secretCount and secretCount[guess[i]] > 0 and guessCount[guess[i]] > 0):
+                print(secretCount)
+                secretCount[guess[i]] -= 1
+                guessCount[guess[i]] -= 1
+                cows+=1
+        # print(bulls,"A",cows,"B")
+
+        return (str(bulls) + 'A' + str(cows) + 'B')
--- 392-is-subsequence.py
+++ 392-is-subsequence.py
@@ -16,3 +16,22 @@ class Solution:
         else:
             return False
                 
+
+#v2 96%
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if(len(s)==0): return True
+        if len(s) > len(t):
+            return False
+        lettersFound = 0
+
+        for i in range(len(t)):
+            if(lettersFound>=len(s)):
+                break
+            if(t[i]==s[lettersFound]):
+                lettersFound +=1
+        if lettersFound == len(s):
+            return True
+        else:
+            return False
