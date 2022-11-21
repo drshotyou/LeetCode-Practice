b76519d1178c6b214bca62804990c502603a2edf
Alden Rivera <alden-rf@hotmail.com>
2022-11-21 00:52:19 -0800

    Update 278-first-bad-version.py

--- 278-first-bad-version.py
+++ 278-first-bad-version.py
@@ -58,3 +58,23 @@ def binSearch(left,right):
             return binSearch(left,mid-1)
     else:
         return binSearch(mid+1,right)
+
+#v5 tried doing it with a loop today
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 0
+        right = n
+        while(left<=right):
+            mid = left + (right - left)//2
+            if(isBadVersion(mid)):
+                if(not isBadVersion(mid-1)):
+                    return mid
+                else:
+                    right = mid -1
+            else:
+                left = mid + 1
+
+                
