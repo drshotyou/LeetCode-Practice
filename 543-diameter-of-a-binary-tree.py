7db2c903fdffad8c2471cfeeeee5831c96c59764
Alden Rivera <alden-rf@hotmail.com>
2023-01-27 15:55:38 -0400

    added 110 543

---
+++ 543-diameter-of-a-binary-tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+
+            left = dfs(root.left)
+            right = difs(root.right)
+            res[0] = max(res[0],2+left+right)
+
+            return 1 + max(left,right)
+        dfs(root)
+        return res[0]
